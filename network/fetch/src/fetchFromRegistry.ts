932aa9142c3b32ddf9799337d749efa7f0f74b6b
liamvnastoria <liam.dispa@skygenesisenterprise.com>
2025-11-14 07:41:20 +0100

    Add network folder

---
+++ network/fetch/src/fetchFromRegistry.ts
@@ -0,0 +1,118 @@
+import { URL } from 'url'
+import { type SslConfig } from '@pnpm/types'
+import { type FetchFromRegistry } from '@pnpm/fetching-types'
+import { getAgent, type AgentOptions } from '@pnpm/network.agent'
+import { fetch, isRedirect, type Response, type RequestInfo, type RequestInit } from './fetch.js'
+
+const USER_AGENT = 'pnpm' // or maybe make it `${pkg.name}/${pkg.version} (+https://npm.im/${pkg.name})`
+
+const FULL_DOC = 'application/json'
+const ACCEPT_FULL_DOC = `${FULL_DOC}; q=1.0, */*`
+
+const ABBREVIATED_DOC = 'application/vnd.npm.install-v1+json'
+const ACCEPT_ABBREVIATED_DOC = `${ABBREVIATED_DOC}; q=1.0, ${FULL_DOC}; q=0.8, */*`
+
+const MAX_FOLLOWED_REDIRECTS = 20
+
+export interface FetchWithAgentOptions extends RequestInit {
+  agentOptions: AgentOptions
+}
+
+export function fetchWithAgent (url: RequestInfo, opts: FetchWithAgentOptions): Promise<Response> {
+  const agent = getAgent(url.toString(), {
+    ...opts.agentOptions,
+    strictSsl: opts.agentOptions.strictSsl ?? true,
+  } as any) as any // eslint-disable-line
+  const headers = opts.headers ?? {}
+  // @ts-expect-error
+  headers['connection'] = agent ? 'keep-alive' : 'close'
+  return fetch(url, {
+    ...opts,
+    agent,
+  })
+}
+
+export type { AgentOptions }
+
+export interface CreateFetchFromRegistryOptions extends AgentOptions {
+  fullMetadata?: boolean
+  userAgent?: string
+  sslConfigs?: Record<string, SslConfig>
+}
+
+export function createFetchFromRegistry (defaultOpts: CreateFetchFromRegistryOptions): FetchFromRegistry {
+  return async (url, opts): Promise<Response> => {
+    const headers = {
+      'user-agent': USER_AGENT,
+      ...getHeaders({
+        auth: opts?.authHeaderValue,
+        fullMetadata: defaultOpts.fullMetadata,
+        userAgent: defaultOpts.userAgent,
+      }),
+    }
+
+    let redirects = 0
+    let urlObject = new URL(url)
+    const originalHost = urlObject.host
+    /* eslint-disable no-await-in-loop */
+    while (true) {
+      const agentOptions = {
+        ...defaultOpts,
+        ...opts,
+        strictSsl: defaultOpts.strictSsl ?? true,
+      } as any // eslint-disable-line
+
+      // We should pass a URL object to node-fetch till this is not resolved:
+      // https://github.com/bitinn/node-fetch/issues/245
+      const response = await fetchWithAgent(urlObject, {
+        agentOptions: {
+          ...agentOptions,
+          clientCertificates: defaultOpts.sslConfigs,
+        },
+        // if verifying integrity, node-fetch must not decompress
+        compress: opts?.compress ?? false,
+        method: opts?.method,
+        headers,
+        redirect: 'manual',
+        retry: opts?.retry,
+        timeout: opts?.timeout ?? 60000,
+      })
+      if (!isRedirect(response.status) || redirects >= MAX_FOLLOWED_REDIRECTS) {
+        return response
+      }
+
+      // This is a workaround to remove authorization headers on redirect.
+      // Related pnpm issue: https://github.com/pnpm/pnpm/issues/1815
+      redirects++
+      urlObject = new URL(response.headers.get('location')!)
+      if (!headers['authorization'] || originalHost === urlObject.host) continue
+      delete headers.authorization
+    }
+    /* eslint-enable no-await-in-loop */
+  }
+}
+
+interface Headers {
+  accept: string
+  authorization?: string
+  'user-agent'?: string
+}
+
+function getHeaders (
+  opts: {
+    auth?: string
+    fullMetadata?: boolean
+    userAgent?: string
+  }
+): Headers {
+  const headers: { accept: string, authorization?: string, 'user-agent'?: string } = {
+    accept: opts.fullMetadata === true ? ACCEPT_FULL_DOC : ACCEPT_ABBREVIATED_DOC,
+  }
+  if (opts.auth) {
+    headers['authorization'] = opts.auth
+  }
+  if (opts.userAgent) {
+    headers['user-agent'] = opts.userAgent
+  }
+  return headers
+}
---
+++ network/fetch/src/index.ts
@@ -0,0 +1,3 @@
+export type { FetchFromRegistry } from '@pnpm/fetching-types'
+export { fetch, type RetryTimeoutOptions } from './fetch.js'
+export { createFetchFromRegistry, fetchWithAgent, type AgentOptions, type CreateFetchFromRegistryOptions } from './fetchFromRegistry.js'
