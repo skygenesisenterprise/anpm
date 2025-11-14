b03809b898c74845173733aff49c8d867408b4f4
liamvnastoria <liam.dispa@skygenesisenterprise.com>
2025-11-14 07:47:53 +0100

    Add catalogs folder

---
+++ catalogs/resolver/src/index.ts
@@ -0,0 +1,11 @@
+export {
+  resolveFromCatalog,
+  type CatalogResolution,
+  type CatalogResolutionFound,
+  type CatalogResolutionMisconfiguration,
+  type CatalogResolutionUnused as CatalogResolutionNotUsed,
+  type CatalogResolutionResult,
+  type CatalogResolver,
+  type WantedDependency,
+} from './resolveFromCatalog.js'
+export { type CatalogResultMatcher, matchCatalogResolveResult } from './matchCatalogResolveResult.js'
